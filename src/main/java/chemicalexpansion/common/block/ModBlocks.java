c07ac78d2bd9f6447920027dbf99ec8c1f910ba0
Alex Grimwood <alexandergrimwood94@gmail.com>
2017-01-19 15:35:46 +0000

    Update ModBlocks.java

--- src/main/java/chemicalexpansion/common/block/ModBlocks.java
+++ src/main/java/chemicalexpansion/common/block/ModBlocks.java
@@ -1,7 +1,7 @@
 package chemicalexpansion.common.block;
 
 import chemicalexpansion.common.block.counter.BlockCounter;
-import chemicalexpansion.common.item.ItemOreDict;
+import chemicalexpansion.common.item.BlockBaseDict;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemBlock;
@@ -45,6 +45,9 @@ public class ModBlocks {
     public static BlockOre ore_apatite;
     public static BlockOre ore_sulfur;
     public static BlockOre ore_pollucite;
+    public static BlockOre ore_thortveitite;
+    public static BlockOre ore_vanadinite;
+    public static BlockOre ore_chromite;
     //
     public static BlockBase block_copper;
     public static BlockBase block_zinc;
@@ -57,26 +60,81 @@ public class ModBlocks {
     public static BlockCounter counter;
 
     public static void init() {
-        // Ores
-        ore_chalcopyrite = register(new BlockOre("ore_chalcopyrite", "oreCopper"));
-        ore_sphalerite = register(new BlockOre("ore_sphalerite", "oreZinc"));
-        ore_ilmenite = register(new BlockOre("ore_ilmenite", "oreTitanium"));
-        ore_garnierite = register(new BlockOre("ore_garnierite", "oreNickel"));
-        ore_galena = register(new BlockOre("ore_galena", "oreLead"));
-        ore_bauxite = register(new BlockOre("ore_bauxite", "oreAluminium"));
-        ore_wolframite = register(new BlockOre("ore_wolframite", "oreTungsten"));
-        ore_pyrolusite = register(new BlockOre("ore_pyrolusite", "oreManganese"));
         // Extension
+        ore_beryl = register(new BlockOre("ore_beryl", "oreBeryllium"));
+        ore_magnesite = register(new BlockOre("ore_magnesite", "oreMagnesium"));
+        ore_bauxite = register(new BlockOre("ore_bauxite", "oreAluminium")); // chance for gallium
+        ore_bauxite = register(new BlockOre("ore_thortveitite", "oreScandium"));
+        ore_ilmenite = register(new BlockOre("ore_ilmenite", "oreTitanium"));
+        ore_vanadinite = register(new BlockOre("ore_vanadinite", "oreVanadium"));
+        ore_chromite = register(new BlockOre("ore_chromite", "oreChromium"));
+        ore_pyrolusite = register(new BlockOre("ore_pyrolusite", "oreManganese"));
+        ore_cobaltite = register(new BlockOre("ore_cobaltite", "oreCobalt"));
+        ore_garnierite = register(new BlockOre("ore_garnierite", "oreNickel"));
+        ore_chalcopyrite = register(new BlockOre("ore_chalcopyrite", "oreCopper")); // selenium crumb
+        ore_sphalerite = register(new BlockOre("ore_sphalerite", "oreZinc")); // chance for germanium, gallium
+        //se
+        //yt
+        //zr
+        //ni
+        //mo
+        //ru
+        //rh
+        //pa
+        ore_argentite = register(new BlockOre("ore_argentite", "oreSilver"));
+        //cd
+        //...
+        ore_wolframite = register(new BlockOre("ore_wolframite", "oreTungsten"));
+        //...
+        ore_galena = register(new BlockOre("ore_galena", "oreLead"));
+        //bi
+
         ore_cinnabar = register(new BlockOre("ore_cinnabar", "oreMercury"));
         // Material Blocks
-        block_copper = register(new BlockBase(Material.IRON, "block_copper"));
-        block_zinc = register(new BlockBase(Material.IRON, "block_zinc"));
-        block_titanium = register(new BlockBase(Material.IRON, "block_titanium"));
-        block_nickel = register(new BlockBase(Material.IRON, "block_nickel"));
-        block_lead = register(new BlockBase(Material.IRON, "block_lead"));
-        block_aluminium = register(new BlockBase(Material.IRON, "block_aluminium"));
-        block_tungsten = register(new BlockBase(Material.IRON, "block_tungsten"));
-        block_manganese = register(new BlockBase(Material.IRON, "block_manganese"));
+        register(block_beryllium = new BlockBase(Material.IRON, "block_beryllium"));       // 4
+        register(block_magnesium = new BlockBase(Material.IRON, "block_magnesium"));       // 12
+        register(block_aluminium = new BlockBase(Material.IRON, "block_aluminium"));       // 13
+        register(block_silicon = new BlockBase(Material.IRON, "block_silicon"));             // 14
+        register(block_scandium = new BlockBase(Material.IRON, "block_scandium"));          // 
+        register(block_titanium = new BlockBase(Material.IRON, "block_titanium"));          // 
+        register(block_vanadium = new BlockBase(Material.IRON, "block_vanadium"));          // 
+        register(block_chromium = new BlockBase(Material.IRON, "block_chromium"));          // 
+        register(block_manganese = new BlockBase(Material.IRON, "block_manganese"));       // 
+        register(block_cobalt = new BlockBase(Material.IRON, "block_cobalt"));                // 
+        register(block_nickel = new BlockBase(Material.IRON, "block_nickel"));                // 
+        register(block_copper = new BlockBase(Material.IRON, "block_copper"));                // 
+        register(block_zinc = new BlockBase(Material.IRON, "block_zinc"));                      // 
+        register(block_gallium = new BlockBase(Material.IRON, "block_gallium"));             // 
+        register(block_germanium = new BlockBase(Material.IRON, "block_germanium"));       // 
+        register(block_selenium = new BlockBase(Material.IRON, "block_selenium"));          // 
+        register(block_yttrium = new BlockBase(Material.IRON, "block_yttrium"));             // 
+        register(block_zirconium = new BlockBase(Material.IRON, "block_zirconium"));       // 
+        register(block_niobium = new BlockBase(Material.IRON, "block_niobium"));             // 
+        register(block_molybdenum = new BlockBase(Material.IRON, "block_molybdenum"));    // 
+        register(block_ruthenium = new BlockBase(Material.IRON, "block_ruthenium"));       // 
+        register(block_rhodium = new BlockBase(Material.IRON, "block_rhodium"));             // 
+        register(block_palladium = new BlockBase(Material.IRON, "block_palladium"));       // 
+        register(block_silver = new BlockBase(Material.IRON, "block_silver"));                // 
+        register(block_cadmium = new BlockBase(Material.IRON, "block_cadmium"));             // 
+        register(block_indium = new BlockBase(Material.IRON, "block_indium"));                // 
+        register(block_tin = new BlockBase(Material.IRON, "block_tin"));                         // 
+        register(block_antimony = new BlockBase(Material.IRON, "block_antimony"));          // 
+        register(block_tellurium = new BlockBase(Material.IRON, "block_tellurium"));       // 
+        register(block_neodymium = new BlockBase(Material.IRON, "block_neodymium"));       // 
+        register(block_hafnium = new BlockBase(Material.IRON, "block_hafnium"));             // 
+        register(block_tantalum = new BlockBase(Material.IRON, "block_tantalum"));          // 
+        register(block_tungsten = new BlockBase(Material.IRON, "block_tungsten"));          // 
+        register(block_rhenium = new BlockBase(Material.IRON, "block_rhenium"));             // 
+        register(block_osmium = new BlockBase(Material.IRON, "block_osmium"));                // 
+        register(block_iridium = new BlockBase(Material.IRON, "block_iridium"));             // 
+        register(block_platinum = new BlockBase(Material.IRON, "block_platinum"));          // 
+        register(block_thallium = new BlockBase(Material.IRON, "block_thallium"));          // 
+        register(block_lead = new BlockBase(Material.IRON, "block_lead"));                      // 
+        register(block_bismuth = new BlockBase(Material.IRON, "block_bismuth"));             // 
+        register(block_americium = new BlockBase(Material.IRON, "block_americium"));       // 
+        register(block_curium = new BlockBase(Material.IRON, "block_curium"));                // 
+        register(block_berkelium = new BlockBase(Material.IRON, "block_berkelium"));       // 
+        register(block_californium = new BlockBase(Material.IRON, "block_californium")); // 
         // Machines
         counter = register(new BlockCounter());
     }
@@ -88,11 +146,11 @@ public class ModBlocks {
         if (block instanceof BlockBase) {
             ((BlockBase)block).registerItemModel(itemBlock);
         }
-        if (block instanceof ItemOreDict) {
-            ((ItemOreDict)block).initOreDict();
+        if (block instanceof BlockBaseDict) {
+            ((BlockBaseDict)block).initOreDict();
         }
-        if (itemBlock instanceof ItemOreDict) {
-            ((ItemOreDict)itemBlock).initOreDict();
+        if (itemBlock instanceof BlockBaseDict) {
+            ((BlockBaseDict)itemBlock).initOreDict();
         }
         if (block instanceof BlockTileEntity) {
             GameRegistry.registerTileEntity(((BlockTileEntity<?>)block).getTileEntityClass(), block.getRegistryName().toString());
